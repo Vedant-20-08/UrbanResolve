5a5593da86eeb9534cd4e079405df2f184b7a1da
Vedant <vedantjariwala2008@gmail.com>
2026-03-23 20:13:01 +0530

    Delete Prac-4i.c

--- Prac-4i.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main() 
-{
-   float F,C;
-   printf("Enter the temperature in Fahrenheit: ");
-   scanf("%f",&F);
-   C=(F-32)*5/9;
-   printf("The temperature in Celsius is: %.2f",C);    
-}
