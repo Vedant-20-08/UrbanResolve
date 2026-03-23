b546e95523abea5c1ec27a3a9a94ffa09e515117
Vedant <vedantjariwala2008@gmail.com>
2026-03-23 20:09:01 +0530

    Delete Prac-2.c

--- Prac-2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main() 
-{
-    float p, r, n;
-    printf("Enter principal, rate of interest and time period: ");    
-    scanf("%f %f %f", &p, &r, &n);
-    float i = p * r * n / 100;
-    printf("The simple interest is: %.2f\n", i);    
-}
